0c231ca8396aada3bb5f7527ae3208056f8dd015
Christos Athanasiadis <christos.k.athanasiadis@gmail.com>
2020-01-21 13:43:52 +0200

    tests

--- tests/OptonTest.php
+++ tests/OptonTest.php
@@ -5,15 +5,36 @@ use PHPUnit\Framework\TestCase;
 use Kristos80\Opton\Opton;
 
 final class OptonOutputTest extends TestCase {
-	protected $pool = array(
+	const DEFAULT_NAME = 'Athanasios';
+	protected $poolOne = array(
 		'name' => 'Chris',
 		'age' => 40,
 		'profession' => 'Web Developer',
+	);
+	protected $acceptedNames = array(
+		'Iole',
+		'Maria',
+		self::DEFAULT_NAME,
 	);
 
 	public function testExpectChris(): void {
 		$this->expectOutputString('Chris');
 
-		echo (new Opton())->get('name', $this->pool);
+		echo (new Opton())->get('name', $this->poolOne);
+	}
+
+	public function testExpectForty(): void {
+		$this->expectOutputString('40');
+
+		echo (new Opton())->get(array(
+			'age',
+			'yearsOld',
+		), (object) $this->poolOne);
+	}
+
+	public function testExpectInvalidName(): void {
+		$this->expectOutputString(self::DEFAULT_NAME);
+
+		echo (new Opton())->get('name', $this->poolOne, self::DEFAULT_NAME, $this->acceptedNames);
 	}
 }
